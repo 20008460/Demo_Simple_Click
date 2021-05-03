9251a99b811e119f89547d9ee8cfcda428f10b2c
20008460 <20008460@myrp.edu.sg>
2021-05-03 09:43:54 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20008460/demosimpleclick/MainActivity.java
@@ -0,0 +1,20 @@
+package sg.edu.rp.c346.id20008460.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //
+        setContentView(R.layout.activity_main); // ".xml" is not needed
+
+
+    }
+
+
+}
